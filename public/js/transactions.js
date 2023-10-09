6073f53be3bb282b34b950012cfc0fcb082aef97
Eder de Araujo Barbosa <eder@eabcomercio.com.br>
2023-10-09 12:52:55 -0300

    Eder 9.10.r2

--- public/js/transactions.js
+++ public/js/transactions.js
@@ -8,6 +8,45 @@ let data = {
 
 document.getElementById("button-logout").addEventListener("click", logout)
 
+//ADICIONAR LANÇAMENTO
+document.getElementById("transaction-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const value = parseFloat(document.getElementById("value-input").value);
+    const description = document.getElementById("description-input").value;
+    const date = document.getElementById("date-input").value;
+    const type = document.querySelector('input[name="type-input"]:checked').value;
+
+    data.transactions.unshift({value: value, type: type, description: description, date: date});
+
+    saveData(data);
+    e.target.reset();
+    myModal.hide();
+
+    alert("Lançamento adicionado com sucesso.");
+});
+
+checkLogged();
+
+
+function checkLogged() {
+    if(session) {
+        sessionStorage.setItem("logged", session);
+        logged = session;
+    }
+
+    if(!logged) {
+        window.Location.href = "index.html";
+        return;
+    }
+
+    const dataUser = localStorage.getItem(logged);
+    if(dataUser) {
+        data = JSON.parse(dataUser);
+    }
+
+}
+
 function logout() {
     sessionStorage.removeItem("logged");
     localStorage.removeItem("session");
